6529a84a7c2474968c41fee13df4ab72626829da
Mark Shipton <mshipton@gmail.com>
2017-03-23 14:19:05 +0000

    WIP

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def webhook():
 def processWhatAmICoveringRequest(req):
     covering = req.get("result").get("parameters").get("covering")
     
-    text = "<speak>I am covered in " + covering + '<audio src="http://www.animal-sounds.org/farm/Cow%20animals055.wav"/></speak>'
+    text = "<speak>I am covered in " + covering + '<audio src="http://www.animal-sounds.org/farm/Cow%20animals055.wav">Moo!</audio></speak>'
     return makeSpeechResponse(text)
 
 
