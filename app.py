d29cdbbd39eb72f9a702c32fc2297f98f9d7da39
Mark Shipton <mshipton@gmail.com>
2017-03-23 15:15:18 +0000

    WIP

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from urllib.error import HTTPError
 
 import json
 import os
+import random
 
 from flask import Flask
 from flask import request
@@ -27,18 +28,50 @@ animals = {
     }
 }
 
+
+class Animal(object):
+    def __init__(self, name, properties):
+        self.name = name
+        self.properties = properties
+
+    def checkCovering(self, input_covering):
+        return input_covering == self.properties['covering']
+
+    def finalGuess(self, input_guess):
+        return input_guess == self.name
+
+animals = []
+animals.append(Animal("dog", {"covering": "hair", "legs": 4}))
+animals.append(Animal("duck", {"covering": "feathers", "legs": 2}))
+
+
+def findAnimal(context):
+    input_name = context['parameters']['answer']
+    return [animal for animal in animals if animal.name == input_name][0]
+
+
+def getContext(req, input_name):
+    for context in req['result']['contexts']:
+        if context['name'] == input_name:
+            return context
+    return None
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
+    context = getContext(req, "whatami")
 
     print("Request:")
     print(json.dumps(req, indent=4))
 
     action = req.get("result").get("action")
+    animal = findAnimal(context) if action != "start" else None
+
     if action == "start":
         res = processStart(req)
-    elif action == "whatAmICovering":
-        res = processWhatAmICoveringRequest(req)        
+    elif action == "covering":
+        res = processCovering(req, animal)        
     else:
         return
 
@@ -50,14 +83,17 @@ def webhook():
 
 def processStart(req):
     text = "I'm am animal, guess what I am!"
-
-    contextOut = [{"name":"whatami", "lifespan":3, "parameters":{"answer": "dog"}}]
+    animal = random.choice(animals)
+    contextOut = {"name":"whatami", "lifespan":3, "parameters":{"answer": animal.name}}
     return makeSpeechResponse(text, contextOut)
 
-def processWhatAmICoveringRequest(req):
+def processCovering(req, animal):
     covering = req.get("result").get("parameters").get("covering")
-    
-    text = "<speak>I am covered in " + covering + ' <audio src="https://www.partnersinrhyme.com/files/sounds1/WAV/sports/baseball/Ball_Hit_Cheer.wav">Moo!</audio></speak>'
+    is_correct = animal.checkCovering(covering)      
+    if is_correct:
+        text = "I am covered in " + covering
+    else:
+        text = "I am not covered in " + covering
     return makeSpeechResponse(text)
 
 def makeContextResponse(contextOut=[]):
